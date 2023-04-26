af6ef875485a005f04db52735427fb05e8f04994
Shrabon Ahamed <shrabonahamed106@gmail.com>
2023-04-26 19:49:02 +0600

    added folder structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
+import { Button } from '@mui/material';
 import './App.css';
-import Button from '@material-ui/core/Button';
 
 function App() {
+
   return (
     <div className="App">
-        <h1>This is App Component</h1>
-        <Button>Meterial ui Button</Button>
-        <Button variant="contained">Default</Button>
-        <Button variant='outlined' color='primary'>Primary</Button>
+        <Button variant='contained'>Click me</Button>
     </div>
   );
 }
