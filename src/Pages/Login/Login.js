aa6bd200521582f4794a4cda796028dd6a229f4d
Shrabon Ahamed <shrabonahamed106@gmail.com>
2023-04-27 19:11:37 +0600

    get form value of react hook form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Card, CardContent, Grid, TextField, Button, Typography, Box } from '@mui/material'
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+
+    const {register, handleSubmit} = useForm();
+    const handleLoginForm = (data)=> {
+        console.log(data)
+    }
+
+
     return (
         <div style={{ padding: '15px' }}>
             <Card sx={{
@@ -22,14 +30,14 @@ const Login = () => {
                     padding: 2,
                     paddingBottom: '10px',
                 }}>
-                    <form action="">
+                    <form onSubmit={handleSubmit(handleLoginForm)}>
                         <Grid container spacing={2}>
 
                             <Grid xs={12} item >
-                                <TextField variant='outlined' label='Email *' placeholder='Enter your Email' fullWidth></TextField>
+                                <TextField variant='outlined' label='Email *' {...register('email', {required: true})} name='email' placeholder='Enter your Email' fullWidth></TextField>
                             </Grid>
                             <Grid xs={12} item >
-                                <TextField variant='outlined' label='Password *' placeholder='Enter your Password' fullWidth></TextField>
+                                <TextField variant='outlined' label='Password *' {...register('password', {required: true})} name='password' type='password' placeholder='Enter your Password' fullWidth></TextField>
                             </Grid>
                             <Typography sx={{
                                 paddingLeft: 2,
@@ -53,9 +61,7 @@ const Login = () => {
                         OR
                     </Typography>
 
-                    <Grid xs={12} item sx={{
-                        // marginTop: 3
-                    }} >
+                    <Grid xs={12} item>
                         <Button type='submit' variant='contained' color='primary' fullWidth sx={{ padding: '15px' }}>Login with Google</Button>
                     </Grid>
                     <Box sx={{
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Card, CardContent, Grid, TextField, Button, Typography, Box } from '@mui/material'
+import {
+    Card, CardContent, Grid, TextField,
+    Button, Typography, Box, FormControl, 
+    InputLabel, Select, MenuItem
+} from '@mui/material'
 import { Link } from 'react-router-dom';
-
+import { useForm } from 'react-hook-form';
 
 const Register = () => {
+    const { register, handleSubmit } = useForm();
+
+    const handleRegisterForm = (data) => {
+        console.log(data);
+    }
+
     return (
         <div style={{ padding: '15px' }}>
             <Card sx={{
@@ -18,24 +28,40 @@ const Register = () => {
                     Register
                 </Typography>
 
-
                 <CardContent sx={{
                     padding: 2,
                     paddingBottom: '10px',
                 }}>
-                    <form action="">
+                    <form onSubmit={handleSubmit(handleRegisterForm)}>
                         <Grid container spacing={2}>
                             <Grid xs={12} item >
-                                <TextField variant='outlined' label='Name *' placeholder='Enter your Name' fullWidth></TextField>
+                                <TextField variant='outlined' label='Name *' {...register('name', { required: true })} name='name' placeholder='Enter your Name' fullWidth></TextField>
                             </Grid>
                             <Grid xs={12} item >
-                                <TextField variant='outlined' label='Photo *' placeholder='Enter your Photo Link' fullWidth></TextField>
+                                <TextField variant='outlined' label='Photo *' {...register('photo', { required: true })} name='photo' placeholder='Enter your Photo Link' fullWidth></TextField>
+                            </Grid>
+                            <Grid xs={12} item>
+                                <FormControl fullWidth  >
+                                    <InputLabel variant='outlined' label='Role' id="demo-simple-select-label" placeholder='Select Your Role'>Role</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        label="Role"
+                                        name='select'
+                                        {...register('select')}
+                                    >
+                                        <MenuItem value={'User'}>User</MenuItem>
+                                        <MenuItem value={'Selar'}>Selar</MenuItem>
+                                        <MenuItem value={'Admin'}>Admin</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+
+                            <Grid xs={12} item >
+                                <TextField variant='outlined' label='Email *' {...register('email', { required: true })} name='email' placeholder='Enter your Email' fullWidth></TextField>
                             </Grid>
                             <Grid xs={12} item >
-                                <TextField variant='outlined' label='Email *' placeholder='Enter your Email' fullWidth></TextField>
-                            </Grid>
-                            <Grid xs={12} item >
-                                <TextField variant='outlined' label='Password *' placeholder='Enter your Password' fullWidth></TextField>
+                                <TextField variant='outlined' label='Password *' {...register('password', { required: true })} name='password' type='password' placeholder='Enter your Password' fullWidth></TextField>
                             </Grid>
                             <Grid xs={12} item >
                                 <Button type='submit' variant='contained' color='primary' fullWidth sx={{ padding: '15px' }}>Register</Button>
@@ -46,7 +72,7 @@ const Register = () => {
                             alignItems: 'center',
                             justifyContent: 'space-between',
                             marginTop: '10px'
-                        
+
                         }}>
 
                             <span>All ready have an account</span>
